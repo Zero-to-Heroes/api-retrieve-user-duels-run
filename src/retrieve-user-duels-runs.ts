9e19374e60794d406907c7dee056b2f6a3a05f40
sebastientromp <sebastien.tromp@gmail.com>
2021-04-29 11:05:44 +0200

    remove error-eating try/catch

--- src/retrieve-user-duels-runs.ts
+++ src/retrieve-user-duels-runs.ts
@@ -15,49 +15,37 @@ export default async (event): Promise<any> => {
 		'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
 		'Access-Control-Allow-Origin': event.headers.Origin || event.headers.origin,
 	};
-	try {
-		console.log('processing event');
-		// Preflight
-		if (!event.body) {
-			const response = {
-				statusCode: 200,
-				body: null,
-				headers: headers,
-			};
-			console.log('sending back success response without body', response);
-			return response;
-		}
-
-		const input: Input = JSON.parse(event.body);
-		const mysql = await getConnection();
-
-		const results = await loadStepResults(mysql, input);
-		const rewardsResults = await loadRewardsResults(mysql, input);
-		await mysql.end();
-
-		const stringResults = JSON.stringify({ results, rewardsResults });
-		const gzippedResults = gzipSync(stringResults).toString('base64');
-		console.log('compressed', stringResults.length, gzippedResults.length);
+	console.log('processing event');
+	// Preflight
+	if (!event.body) {
 		const response = {
 			statusCode: 200,
-			isBase64Encoded: true,
-			body: gzippedResults,
-			headers: {
-				'Content-Type': 'text/html',
-				'Content-Encoding': 'gzip',
-			},
-		};
-		console.log('sending back success reponse');
-		return response;
-	} catch (e) {
-		console.error('issue getting runs info', e);
-		const response = {
-			statusCode: 500,
-			isBase64Encoded: false,
 			body: null,
 			headers: headers,
 		};
-		console.log('sending back error reponse', response);
+		console.log('sending back success response without body', response);
 		return response;
 	}
+
+	const input: Input = JSON.parse(event.body);
+	const mysql = await getConnection();
+
+	const results = await loadStepResults(mysql, input);
+	const rewardsResults = await loadRewardsResults(mysql, input);
+	await mysql.end();
+
+	const stringResults = JSON.stringify({ results, rewardsResults });
+	const gzippedResults = gzipSync(stringResults).toString('base64');
+	console.log('compressed', stringResults.length, gzippedResults.length);
+	const response = {
+		statusCode: 200,
+		isBase64Encoded: true,
+		body: gzippedResults,
+		headers: {
+			'Content-Type': 'text/html',
+			'Content-Encoding': 'gzip',
+		},
+	};
+	console.log('sending back success reponse');
+	return response;
 };
